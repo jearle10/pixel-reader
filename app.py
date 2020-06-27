d12ac6181b1be6b204b8136db7b0b2c181886e58
Jian Earle <jianearle93@googlemail.com>
2020-06-27 15:38:43 +0100

    Added a fileupload and save to img folder

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './static/img/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('home.html')
+    if request.method == "GET":
+        return render_template('home.html')
+
+    if request.method == 'POST':
+        print(request.files)
+        f = request.files['file']
+        # filename = secure_filename(f.filename)
+
+        # f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        f.save("./static/img/" + f.filename)
+
+        return redirect(request.url)
 
 
 if __name__ == '__main__':
